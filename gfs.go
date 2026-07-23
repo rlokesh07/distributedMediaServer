25a26f2a6bfd40b079854c8711c439fc2ee2a5ba
alarcafil <rishilokesh07@gmail.com>
2026-07-22 22:20:11 -0400

    added chunk deletion and replication code

--- gfs.go
+++ gfs.go
@@ -17,13 +17,14 @@ type Master struct {
 	chunkServers string
 	serverEndpoints map[string]*ClientEnd
 
-	lastHeard map[string]time.Time
+	spaceRemaining map[string]int
 
-	// TODO: persister for persistant states
+	lastHeard map[string]time.Time
 
 	leases map[string]*Lease // chunkID to lease
 	versions map[string]int // chunk to version
-	
+	replicas map[string]int
+
 	killCh chan bool
 
 	leasePeriod int // in seconds
@@ -48,7 +49,7 @@ func(ms *Master) service() {
 			return;
 		}
 		
-
+		ms.BroadcastHeartbeat()	
 
 	}
 }
@@ -109,11 +110,57 @@ func(ms *Master) BroadcastHeartbeat() {
 		for chunkId, v := range rpl.versions {
 			if v != ms.versions[chunkId] {
 				
-				// TODO: garbageCollect[chunkId]
+				deleteArgs := DeleteArgs{chunkId: chunkId} 
+				deleteRpl := DeleteRpl {}
+
+				okDel := end.Call("ChunkServer.DeleteChunk", deleteArgs, deleteRpl)
+				if !okDel {} // TODO: retry logic	
+				for i, v := range ms.chunkToServer[chunkId]{
+					if v == serverId{
+						ms.chunkToServer[chunkId] =  append(ms.chunkToServer[chunkId][:i], ms.chunkToServer[chunkId][i+1:]...)
+					}
+				}
+
+
+	
+				servers := ms.chunkToServer[chunkId]
+
+				idx := rand.Intn(len(servers))
+				
+				replicateArgs := ReplicateStartArgs{ chunkId: chunkId, server: ms.serverEndpoints[servers[idx]]}
+				replicateRpl := ReplicateRpl{}
+
+				okReplicate := end.Call("ChunkServer.StartReplicate", replicateArgs, replicateRpl)
+
+				if !okReplicate {} //TODO: retry logic
+
+				/* THOUGHTS: 
+
+				First, I know that the retry logic is wack but it should work on the principle that 
+				the correct version number should persist until the others find this but this could
+				take a few cycles
+
+				Second, the replicate and delete should be put into their own methods in the master
+				namespace. With retry logic this is going to be too much. There is already too much
+				colliding variable names
+				
+				Third, i am potentially worried about the amount of maps im using since it turns 
+				everything into a O(n log(n)) operation. Gut says realistically I won't be running
+				this with more than 20 replicas max which represent like 3 times speed improvement
+				but oh well
+
+				Fourth, I think we should seperate some of the structs into the rpc file this is
+				like a 500 line doc as is
+
+				*/
+				
+				
 				// TODO: replicate[chunkId]
 			
 			}
 		}
+
+		ms.spaceRemaining[serverId] = rpl.spaceRemaining
 
 	}
 
@@ -174,13 +221,15 @@ type ChunkServer struct {
 	master *ClientEnd
 	chunks map[string]Chunk
 	leases map[string]*Lease
+
+	spaceRemaining int
 }
 
 type ReadChunkArgs struct {
 	chunkId string
 	start int
 	end int
-}
+} 
 
 type ReadChunkRpl struct {
 	data []byte
@@ -195,6 +244,7 @@ type HeartBeatArgs struct{}
 
 type HeartBeatRpl struct{
 	versions map[string]int
+	spaceRemaining int
 }
 
 func(cs *ChunkServer) Heartbeat(args *HeartBeatArgs, rpl *HeartBeatRpl) {
@@ -204,6 +254,8 @@ func(cs *ChunkServer) Heartbeat(args *HeartBeatArgs, rpl *HeartBeatRpl) {
 	for _, c := range cs.chunks {
 		rpl.versions[c.id] = c.version
 	}
+
+	rpl.spaceRemaining = cs.spaceRemaining
 }
 
 // SendChunksArgs
@@ -232,3 +284,53 @@ func(cs *ChunkServer) SetLease(args *SetLeaseArgs, rpl *SetLeaseRpl) {
 }
 
 
+
+// Garbage Collector
+type DeleteArgs struct {
+	chunkId string
+}
+
+type DeleteRpl struct {}
+
+func(cs *ChunkServer) DeleteChunk(args *DeleteArgs, rpl *DeleteRpl) {
+	path :=  cs.id + "/" + args.chunkId
+	err := os.Remove(path)
+
+	if err != nil {
+		println("problem deleting chunk")
+		return
+	}
+
+	delete(cs.chunks, args.chunkId)
+	cs.leases[args.chunkId] = nil
+}
+
+
+type ReplicateStartArgs struct {
+	chunkId string
+	server *ClientEnd
+}
+
+type ReplicateStartRpl struct {
+}
+
+
+func(cs *ChunkServer) StartReplicate(args *ReplicateStartArgs, rpl *ReplicateStartRpl){
+	chunk := cs.chunks[args.chunkId]	
+	replicateArgs := ReplicateArgs{chunk: chunk}
+	replicateRpl := ReplicateRpl{}
+
+	args.server.Call("ChunkServer.Replicate", replicateArgs, replicateRpl)
+}
+
+type ReplicateArgs struct {
+	chunk Chunk
+}
+
+type ReplicateRpl struct {}
+
+func (cs *ChunkServer) Replicate(args *ReplicateArgs, rpl *ReplicateRpl) {
+	chunk := args.chunk
+
+	cs.chunks[chunk.id] = chunk
+}
